3b078c4ff8522a37b76f6bf14fc911469e580dd9
vorner <vorner@adminer>
2026-06-06 22:44:28 +0300

    asdasd

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,16 @@
 <?php
 require 'auth.php';
+require 'monitor.php';
 checkAuth();
 
-echo "Секретные данные доступны только авторизованным.";
+$status = getMinecraftStatus('192.168.0.71', 25565); // Укажи IP твоего сервера
 ?>
-<a href="logout.php">Выйти</a>
+
+<h1>Мониторинг сервера</h1>
+<?php if ($status['online'] !== false): ?>
+    <p>Статус: Онлайн</p>
+    <p>Игроки: <?php echo $status['players']['online']; ?> / <?php echo $status['players']['max']; ?></p>
+    <p>Версия: <?php echo $status['version']['name']; ?></p>
+<?php else: ?>
+    <p style="color:red;">Сервер офлайн</p>
+<?php endif; ?>
