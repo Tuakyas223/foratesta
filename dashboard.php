9796c471f21a41ac1f5853355405dbf07e51b8a9
vorner <vorner@adminer>
2026-06-06 22:54:13 +0300

    asdqw

--- dashboard.php
+++ dashboard.php
@@ -1,16 +1,18 @@
 <?php
 require 'auth.php';
-require 'monitor.php';
 checkAuth();
-
-$status = getMinecraftStatus('192.168.0.71', 25565); // Укажи IP твоего сервера
+# echo "Секретные данные доступны только авторизованным.";
 ?>
 
-<h1>Мониторинг сервера</h1>
-<?php if ($status['online'] !== false): ?>
-    <p>Статус: Онлайн</p>
-    <p>Игроки: <?php echo $status['players']['online']; ?> / <?php echo $status['players']['max']; ?></p>
-    <p>Версия: <?php echo $status['version']['name']; ?></p>
-<?php else: ?>
-    <p style="color:red;">Сервер офлайн</p>
-<?php endif; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DashBoard</title>
+</head>
+<body>
+    <h1>Добро пожаловать в DashBoard!</h1>
+    <p>Здесь ты можешь увидеть секретные данные, доступные только авторизованным пользователям.</p>
+</body>
+</html>
